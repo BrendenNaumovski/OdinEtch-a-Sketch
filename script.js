4ef24b17a2c213977fc8f64c95e1ee078e57deb2
Brenden Naumovski <bnaumovski@proton.me>
2025-02-18 16:17:07 -0500

    Added details from extra credit challenges.

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
+function createGrid() {
+    grid.innerHTML = '';
+    for (let i = 0; i < gridSize ** 2; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `${100 / gridSize}%`;
+        square.addEventListener('mouseover', mouseOverSquare);
+        grid.appendChild(square);
+    }
+}
+
+function setGridSize() {
+    let newSize = prompt('Enter new grid size (1-100):');
+    while (!(newSize > 0 && newSize <= 100)) {
+        if (newSize === null) {
+            return;
+        }
+        newSize = prompt('Please enter a valid number between 1 and 100:');
+    }
+    gridSize = newSize;
+    createGrid();
+}
+
 function mouseOverSquare() {
     this.style.backgroundColor = 'black';
 }
 
-const gridSize = 16;
+let gridSize = 16;
 
 const grid = document.querySelector('.grid');
 
-for (let i = 0; i < gridSize; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for (let i = 0; i < gridSize; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.addEventListener('mouseover', mouseOverSquare);
-        row.appendChild(square);
-    }
-    grid.appendChild(row);
-}
+createGrid();
